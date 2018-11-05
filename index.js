7c64ca4e54811ad9fc688a36995041786497ec1a
Andrew Pham <ATP@ATP-Macbook.local>
2018-11-05 13:04:19 -0800

    Add about route and view. Strip heroku text.

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+
+  // ---- ROUTES ---- //
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/about', (req, res) => res.render('pages/about'))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
